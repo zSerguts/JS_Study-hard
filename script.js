81e649c67fb41f3454a6879190671c6786ce9178
Zhenya Serguts <z.serguts@gmail.com>
2021-02-08 01:10:03 +0300

    feat(sctipt.js): new feat

--- script.js
+++ script.js
@@ -1,52 +1,24 @@
-'use strict';
+'use strict'; 
 
-//1
-const lang = prompt('Выберите язык');
-const week = [
-    ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье."], 
-    ["Monday, Tuesday, Wednesday, Thursday, Friday, Sunday, Saturday."]
-];
-const miss = "На таком языке мы не говорим.";
-
-//a
-if (lang === "ru"){
-    console.log (week[0]);
-} 
-else if (lang === "en"){
-    console.log (week[1]);
-}
-else {
-    console.log(miss);
-}
+let now = new Date();
 
 
-//b
-switch(lang){
-    case "ru":
-        console.log (week[0]);
-        break;
-    case "en":
-        console.log (week[1]);
-        break;
-    default:
-        console.log(miss);
-}
-
-//через тернарку
-
-console.log(lang === "ru" ? week[0] : lang === "en" ? week[1] : miss);
+let week = ["Понедельник", "Втроник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
 
 
-//c
-const data = [];
 
-data.ru = week[0];
-data.en = week[1];
+week.forEach (function (item, index, array) {
 
-console.log(String(data[lang]));
+    if (index - 6 === now.getDay() )
+    {
+        console.log (item.bold());
+    }
+    else if (item === "Суббота" || item === "Воскресенье"){
+        console.log (item.italics());
+    }
+    else{
+       console.log (item); 
+    }
+    
 
-
-//2
-
-const namePerson = prompt('Введите имя:');
-console.log (namePerson === 'Артём' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "такого человека нет.");
+});

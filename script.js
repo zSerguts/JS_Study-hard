fdb94e4499d83ef84da554c058505f456570c8ff
Zhenya Serguts <z.serguts@gmail.com>
2021-02-07 19:35:35 +0300

    feat(script.js): new feat

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-let num = 266219;
-let proiz = 1;
+'use strict';
 
-for (let i = 0; i < 6; i++)
-{
-    proiz *= num % 10;
-    num = Math.floor (num / 10); 
+//1
+const lang = prompt('Выберите язык');
+const week = [
+    ["Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье."], 
+    ["Monday, Tuesday, Wednesday, Thursday, Friday, Sunday, Saturday."]
+];
+const miss = "На таком языке мы не говорим.";
+
+//a
+if (lang === "ru"){
+    console.log (week[0]);
+} 
+else if (lang === "en"){
+    console.log (week[1]);
 }
-console.log (proiz);
-proiz *= proiz * proiz;
+else {
+    console.log(miss);
+}
 
-proiz = String(proiz).slice(0,2);
-console.log (proiz);
+
+//b
+switch(lang){
+    case "ru":
+        console.log (week[0]);
+        break;
+    case "en":
+        console.log (week[1]);
+        break;
+    default:
+        console.log(miss);
+}
+
+//через тернарку
+
+console.log(lang === "ru" ? week[0] : lang === "en" ? week[1] : miss);
+
+
+//c
+const data = [];
+
+data.ru = week[0];
+data.en = week[1];
+
+console.log(String(data[lang]));
+
+
+//2
+
+const namePerson = prompt('Введите имя:');
+console.log (namePerson === 'Артём' ? "директор" : namePerson === 'Максим' ? "преподаватель" : "такого человека нет.");
